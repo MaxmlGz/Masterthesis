4d189eb9fa8ad985303e08f93ed40d2554f9564c
MaxmlGz <maximilian.gumz@gmail.com>
2021-05-11 18:21:38 +0200

    creation of "filter"-lists to control for year of incorporation / discontinuation

--- Script2.R
+++ Script2.R
@@ -38,6 +38,43 @@ NodelistALL <- NodelistALL %>%
   )
 
 
+Date.Of.Incorporation.List <- vector(mode = "list")
+
+Date.Of.Incorporation.List <- lapply(1:length(Edgelist.List), function (x) {as.data.frame(sapply(Edgelist.List[[x]], function (y) NodelistALL$CompanyStart [match(y, NodelistALL$CompanyBvDID)]))})
+
+DateOfDiscontinuationTemp <- subset(NodelistALL, str_detect(CompanyStatus, "Active") == FALSE)
+
+Date.Of.Discontinuation.List <- vector(mode = "list")
+
+Date.Of.Discontinuation.List <- lapply(1:length(Edgelist.List), function (x) {as.data.frame(sapply(Edgelist.List[[x]], function (y) DateOfDiscontinuationTemp$CompanyEnd [match(y, DateOfDiscontinuationTemp$CompanyBvDID)]))})
+
+rm(DateOfDiscontinuationTemp)
+
+
+
+Filter.Incorporation <- lapply(1:length(Date.Of.Incorporation.List), function (x) {as.data.frame(sapply(1:ncol(Date.Of.Incorporation.List[[x]]), function (y) Date.Of.Incorporation.List[[x]][,y] <= (2022-x)))})
+
+for (i in 1:length(Filter.Incorporation)) {
+  
+  Filter.Incorporation[[i]][is.na(Filter.Incorporation[[i]])] <- TRUE
+  
+}
+
+
+
+Filter.Discontinuation <- lapply(1:length(Date.Of.Discontinuation.List), function (x) {as.data.frame(sapply(1:ncol(Date.Of.Discontinuation.List[[x]]), function (y) Date.Of.Discontinuation.List[[x]][,y] >= (2022-x)))})
+
+for (i in 1:length(Filter.Discontinuation)) {
+  
+  Filter.Discontinuation[[i]][is.na(Filter.Discontinuation[[i]])] <- TRUE
+  
+}
+
+
+
+
+
+
 
 
 
